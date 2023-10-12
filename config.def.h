96b34caa119f9976c090fec89640c163831117c3
FW <fw@picture-vision.com>
2023-10-12 23:27:35 +0200

    changed font

--- config.def.h
+++ config.def.h
@@ -4,9 +4,13 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Inconsolata:size=12:antialias=true:autohint=true"
+	"CaskaydiaCove Nerd Font:size=12:antialias=true:autohint=true"
 	//"monospace:size=10"
 };
+// static const char *fonts[] = {
+// 	"Inconsolata:size=12:antialias=true:autohint=true"
+// 	//"monospace:size=10"
+// };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
